5ae047f5b52355b37687789034b72088e051d8f6
aetherwalker <aetherwalker@refugesystems.net>
2021-05-23 03:24:45 -0400

    Fixing sync bug
    [finishes #178258142]

--- app/universe/player/index.js
+++ app/universe/player/index.js
@@ -196,7 +196,7 @@ class PlayerConnection extends EventEmitter {
 			if(manager) {
 				for(x=0; x<manager.fields.length; x++) {
 					field = manager.fields[x];
-					if(change[field.id] && !field.attribute.master_only && !field.attribute.server_only) {
+					if(change[field.id] !== undefined && !field.attribute.master_only && !field.attribute.server_only) {
 						send[field.id] = change[field.id];
 					}
 				}
