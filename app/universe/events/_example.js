3a7383963694d9ab1adcd28ca030a43dddcad4ed
aetherwalker <aetherwalker@refugesystems.net>
2021-05-23 04:45:05 -0400

    ...

--- app/universe/events/_example.js
+++ app/universe/events/_example.js
@@ -8,6 +8,9 @@
  * @param {Integer} event.sent Timestamp of when the UI sent the event (By the User's time)
  * @param {RSObject} event.player That triggered the event
  * @param {Object} event.message The payload from the UI
+ * @param {Object} event.message.type Original event type indicated by the UI
+ * @param {Object} event.message.sent The timestamp at which the event was sent by the UI (By the User's time)
+ * @param {Object} event.message.data Typical location of data from the UI
  */
 
 /*
